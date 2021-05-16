950fb3923248ccc754c55ecf6534ec6dcd2c91f6
김형준 <gudwns026@naver.com>
2021-05-16 21:26:01 +0900

    Add Server Model Conversation

---
+++ server/src/main/java/com/java/chat/model/Conversation.java
@@ -0,0 +1,17 @@
+package com.java.chat.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Conversation {
+    String partner;
+    List<MessageModel> messageList;
+}
