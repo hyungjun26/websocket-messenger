216d1f249a3be2b24801acc54318edc07d14eff8
김형준 <gudwns026@naver.com>
2021-05-17 22:46:32 +0900

    Update Server WebsocketConfiguration CORS pattern

--- server/src/main/java/com/java/chat/configuration/WebsocketConfiguration.java
+++ server/src/main/java/com/java/chat/configuration/WebsocketConfiguration.java
@@ -1,5 +1,6 @@
 package com.java.chat.configuration;
 
+import com.java.chat.handler.CustomHandshakeHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -11,12 +12,12 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 public class WebsocketConfiguration implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat").setAllowedOrigins("http://localhost:3000").withSockJS();
+        registry.addEndpoint("/chat").setAllowedOriginPatterns("*").setHandshakeHandler(new CustomHandshakeHandler()).withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
-        registry.enableSimpleBroker("/topic");
+        registry.enableSimpleBroker("/topic","/queue");
     }
 }
