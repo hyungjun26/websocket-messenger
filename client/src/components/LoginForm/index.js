e9754fb12d4833bf18b2e83e9797a5d9f53784b7
김형준 <gudwns026@naver.com>
2021-05-18 01:53:19 +0900

    Update Client LoginForm

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import './LoginForm.css'
+import Button from '@material-ui/core/Button';
 
 export default function LoginForm({setNickname, setLogin}) {
 
@@ -6,10 +8,18 @@ export default function LoginForm({setNickname, setLogin}) {
         setLogin(true);
     }
 
+    const register = () =>{
+        console.log("register!");
+    }
+
     return (
-        <div>
-            <input onChange={e=>setNickname(e.target.value)}/>
-            <button onClick={enterMessanger}>Enter</button>
+        <div className="form-container">
+            <p className="title">Messanger</p>
+            <input className="login-input" onChange={e=>setNickname(e.target.value)} placeholder="ID"/>
+            <input className="login-input" placeholder="Password"/>
+            <button className="btn-login" onClick={enterMessanger}>Enter</button>
+            <div className="hr"/>
+            <button onClick={register} className="register">Register as a member?</button>
         </div>
     )
 }
