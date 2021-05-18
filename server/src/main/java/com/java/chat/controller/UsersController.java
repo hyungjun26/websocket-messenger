2ab3b9a9414ff607522f0a703fbce48be9d5f235
김형준 <gudwns026@naver.com>
2021-05-18 15:28:04 +0900

    Update Server UserController

--- server/src/main/java/com/java/chat/controller/UsersController.java
+++ server/src/main/java/com/java/chat/controller/UsersController.java
@@ -1,9 +1,12 @@
 package com.java.chat.controller;
 
+import com.java.chat.model.Conversation;
+import com.java.chat.storage.ConversationStorage;
 import com.java.chat.storage.UserStorage;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Set;
 
 @RestController
@@ -13,16 +16,17 @@ public class UsersController {
     @GetMapping("/register/{userName}")
     public ResponseEntity<Void> register(@PathVariable String userName){
         System.out.println("handling register user request:");
-//        try{
-//            UserStorage.getInstance().setUsers(userName);
-//        }catch (Exception e){
-//            return ResponseEntity.badRequest().build();
-//        }
+        try{
+            UserStorage.getInstance().setUsers(userName);
+        }catch (Exception e){
+            return ResponseEntity.badRequest().build();
+        }
         return ResponseEntity.ok().build();
     }
 
-    @GetMapping("/fetchAllUsers")
-    public Set<String> fetchAll(){
-        return UserStorage.getInstance().getUsers();
+    @GetMapping("/fetchAllUsers/{userName}")
+    public List<Conversation> fetchAll(@PathVariable String userName){
+        List<Conversation> users = ConversationStorage.getInstance().getConversation(userName);
+        return users;
     }
 }
