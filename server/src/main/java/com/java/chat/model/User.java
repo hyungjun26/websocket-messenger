fb0fd2504d14c1f6970b87da2ad6e41d124c4eee
김형준 <gudwns026@naver.com>
2021-05-19 17:43:33 +0900

    Update Server User Model

--- server/src/main/java/com/java/chat/model/User.java
+++ server/src/main/java/com/java/chat/model/User.java
@@ -1,10 +1,14 @@
 package com.java.chat.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class User {
     private String id;
     private String name;
