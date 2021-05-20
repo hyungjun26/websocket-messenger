1a50c7a93c559e8ed99ebb147179f62f4c2fb5fc
김형준 <gudwns026@naver.com>
2021-05-20 22:26:41 +0900

    Add Server UserModel timestamp

--- server/src/main/java/com/java/chat/model/MessageModel.java
+++ server/src/main/java/com/java/chat/model/MessageModel.java
@@ -1,11 +1,19 @@
 package com.java.chat.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class MessageModel {
     private String message;
     private String author;
+    private String to;
+    private Date timestamp;
 }
