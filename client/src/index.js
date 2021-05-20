eacb4cc4cd9e5729c15d40896d9922c8a454ae4c
김형준 <gudwns026@naver.com>
2021-05-20 22:24:13 +0900

    Update Client Redux

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom';
 import './assets/index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 ReactDOM.render(
-    <App/>,
+    <Provider store={store}>
+        <App/>
+    </Provider>,
     document.getElementById('root')
 );
 
