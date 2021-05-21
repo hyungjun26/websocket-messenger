cd34fd57e9ab5a99e2eb971f5f9e715bd40337f6
김형준 <gudwns026@naver.com>
2021-05-21 23:32:34 +0900

    Update Server MessageController send logic

--- server/src/main/java/com/java/chat/controller/MessageController.java
+++ server/src/main/java/com/java/chat/controller/MessageController.java
@@ -1,10 +1,13 @@
 package com.java.chat.controller;
 
+import com.java.chat.model.Conversation;
 import com.java.chat.model.MessageModel;
 import com.java.chat.storage.UserStorage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.lang.Nullable;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -15,6 +18,8 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 public class MessageController {
 
@@ -22,6 +27,9 @@ public class MessageController {
 
     @Autowired
     private SimpMessagingTemplate simpMessagingTemplate;
+
+    @Autowired
+    private RedisTemplate<String, Conversation> conversationTemplate;
 
     @MessageMapping("/send/{to}")
     public void sendMessage(@DestinationVariable String to, MessageModel message){
@@ -35,6 +43,25 @@ public class MessageController {
     @MessageMapping("/send")
     public void SendToMessage(MessageModel msg){
         logger.info("{}", msg);
+        HashOperations<String, String, Conversation> ho = conversationTemplate.opsForHash();
+        if(ho.hasKey(msg.getAuthor(),msg.getTo())){ // 상대방과 대화 데이터가 있을때
+            Conversation con = ho.get(msg.getAuthor(), msg.getTo());
+            con.getMessageList().add(msg);
+            ho.put(msg.getAuthor(),msg.getTo(),con);
+        } else { // 상대방 데이터가 없을때
+            Conversation newCon = new Conversation(msg.getTo(), new ArrayList<>());
+            newCon.getMessageList().add(msg);
+            ho.put(msg.getAuthor(),msg.getTo(),newCon);
+        }
+        if(ho.hasKey(msg.getTo(), msg.getAuthor())){ // 상대방에게 대화 데이터가 있을때
+            Conversation con = ho.get(msg.getTo(), msg.getAuthor());
+            con.getMessageList().add(msg);
+            ho.put(msg.getTo(), msg.getAuthor(),con);
+        } else { // 상대방에게 대화 데이터가 없을때
+            Conversation newCon = new Conversation(msg.getAuthor(), new ArrayList<>());
+            newCon.getMessageList().add(msg);
+            ho.put(msg.getTo(), msg.getAuthor(),newCon);
+        }
         simpMessagingTemplate.convertAndSend("/topic/"+msg.getTo() , msg);
     }
 
